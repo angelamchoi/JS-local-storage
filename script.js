8095a321a0e0be402904945268728d021f977ace
Angela Choi <angelamchoi@gmail.com>
2023-04-07 09:21:21 -0400

    worked on adding localstorage for items

---
+++ script.js
@@ -0,0 +1,52 @@
+const addItems = document.querySelector('.add-items');
+  const itemsList = document.querySelector('.plates');
+  const items = JSON.parse(localStorage.getItem('items')) || [];
+
+  function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value; // narrowing down to specific input
+    const item = {
+      text,
+      done: false
+    };
+
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+  }
+
+  const populateList = (plates = [], platesList) => {
+    platesList.innerHTML = plates.map((plate, i) => {
+      return `
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+          <label for="item${i}">${plate.text}</label>
+        </li>
+      `;
+    }).join('');
+  }
+
+  const toggleDone = (e) => {
+    if (!e.target.matches('input')) return; 
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+
+  addItems.addEventListener('submit', addItem);
+  itemsList.addEventListener('click', toggleDone);
+
+  populateList(items, itemsList);
+
+
+  // Notes:
+  // LocalStorage
+  // - localStorage is a property on the window object
+  // - you have access to getItem, setItem, removeItem
+  // JSON.stringify to make it a string
+  // JSON.parse to make it an object
+  // can't use the keyword 'this' in an arrow function
+
